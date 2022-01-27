7268f9d9c2483f730b2a17dffa99574f94afc904
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-01-27 08:58:03 +0000

    Added a fail message in if there are no bike available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,22 +1,34 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
+  describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike) }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    #it 'releases working bikes' do
+     # bike = subject.release_bike
+     # expect(bike).to be_working
+    #end
+
+    it 'raises an error when there are no bike available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
-
+  
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to(:bike) }
 
-  it 'docks something' do
-    bike = Bike.new
+ # it 'docks something' do
+  #  bike = Bike.new
     #Below is because we want to return the bike we dock
-    expect(subject.dock(bike)).to eq bike
-  end
+   # expect(subject.dock(bike)).to eq bike
+  #end
   
   it 'returns docked bikes' do
     bike = Bike.new
@@ -24,6 +36,8 @@ describe DockingStation do
     #below because we need to return the bike we just docked
     expect(subject.bike).to eq bike
   end
+
+
 
 
 end
